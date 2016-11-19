de9b023d8b15968172bd2a927d4c77d6622b7caf
yangjinpeng <yangjinpeng@maijia.com>
2016-11-19 12:13:46 +0800

    【Superman】文件修改

--- 1_all_code_json_mongo.sh
+++ 1_all_code_json_mongo.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # 启动进程
 cd /root/worksapce/Superman/com/alex/strategy
-nohup python all_code_json_mongo.py &
+nohup python all_code_json_mongo.py > all_code.log 2>&1 &
--- 2_b_code_json_mongo_email.sh
+++ 2_b_code_json_mongo_email.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # 启动进程
 cd /root/worksapce/Superman/com/alex/strategy
-nohup python b_code_json_mongo_email.py &
+nohup python b_code_json_mongo_email.py > b_code.log 2>&1 &
