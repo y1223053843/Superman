14e6742528d651f502103b9f5d3bcc32b7988a89
yangjinpeng <yangjinpeng@maijia.com>
2016-11-30 21:45:37 +0800

    【Superman】文件修改

--- 1_all_code_json_mongo.sh
+++ 1_all_code_json_mongo.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 # 启动进程
+
+PATH=/root/anaconda2/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/root/bin
+export PATH
+
 cd /root/worksapce/Superman/com/alex/strategy
 nohup python all_code_json_mongo.py > all_code.log 2>&1 &
 tail -f all_code.log
--- 2_b_code_json_mongo_email.sh
+++ 2_b_code_json_mongo_email.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 # 启动进程
+PATH=/root/anaconda2/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/root/bin
+export PATH
 cd /root/worksapce/Superman/com/alex/strategy
 nohup python b_code_json_mongo_email.py > b_code.log 2>&1 &
 tail -f b_code.log
