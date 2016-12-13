259e9d757883c64e5f299c4c38eab0ed3cc76e18
yangjinpeng <yangjinpeng@maijia.com>
2016-12-13 22:02:31 +0800

    【Superman】文件修改

--- com/alex/strategy/all_code_json_mongo.py
+++ com/alex/strategy/all_code_json_mongo.py
@@ -33,7 +33,6 @@ def execute():
     count = 0
     all_code_index_x = num.array(all_code_index)
     zhishu_code_index = num.array(['399006','399001','000001'])
-
     all = num.concatenate((all_code_index_x,zhishu_code_index))
     for codeItem in all:
         #if (count == 100):
