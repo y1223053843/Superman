441523a4aa875bf042e59579a9dc0a4123fffd71
yangjinpeng <yangjinpeng@maijia.com>
2017-01-10 12:14:44 +0800

    【Superman】文件修改

--- com/alex/strategy/gold_code_json_mongo_email.py
+++ com/alex/strategy/gold_code_json_mongo_email.py
@@ -97,5 +97,5 @@ else:
     print time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())) +  '=====gold_code_json_mongo_email Start====='
     remove(collectionName)
     execute()
-    toDataFrame_param({}, 'Zhishu_Code_JSON_Mongo', collectionName)
+    toDataFrame_param({}, 'Gold_Code_JSON_Mongo', collectionName)
     print time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())) +  '=====gold_code_json_mongo_email End====='
