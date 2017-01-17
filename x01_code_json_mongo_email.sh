78c7563b97f08a91ad129084f1e5dc9547b8fac4
yangjinpeng <yangjinpeng@maijia.com>
2017-01-17 19:24:01 +0800

    【Superman】文件修改

--- x01_code_json_mongo_email.sh
+++ x01_code_json_mongo_email.sh
@@ -3,4 +3,4 @@
 PATH=/root/anaconda2/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/root/bin
 export PATH
 cd /root/worksapce/Superman/com/alex/strategy
-nohup python x01_code_json_mongo_email.py > gold_code.log 2>&1 &
+nohup python x01_code_json_mongo_email.py > x01_code.log 2>&1 &
--- x02_code_json_mongo_email.sh
+++ x02_code_json_mongo_email.sh
@@ -3,4 +3,4 @@
 PATH=/root/anaconda2/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/root/bin
 export PATH
 cd /root/worksapce/Superman/com/alex/strategy
-nohup python x02_code_json_mongo_email.py > gold_code.log 2>&1 &
+nohup python x02_code_json_mongo_email.py > x02_code.log 2>&1 &
