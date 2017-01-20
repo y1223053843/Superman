e8bed37a025761d2320efa8a8ce624d751c9465d
yangjinpeng <yangjinpeng@maijia.com>
2017-01-20 14:10:25 +0800

    【Superman】文件修改

--- com/alex/strategy/zhishu_code_json_mongo_email.py
+++ com/alex/strategy/zhishu_code_json_mongo_email.py
@@ -53,7 +53,7 @@ def execute():
                 i = 0
                 for i in [0] :
                     xinhao = xinhao + codeItem + '买入信号出现：' +  mairuresult_D + ' ' + mairuresult_60 + ' ' + mairuresult_bl_60 + ' '
-                    email_util.sendMail(codeItem + '买入信号出现：' +  mairuresult_60 + ' ' + mairuresult_bl_60, '买入，谨慎谨慎再谨慎')
+                    email_util.sendMail(codeItem + '买入信号出现：' +  mairuresult_60 + ' ' + mairuresult_bl_60, codeItem + '买入，谨慎谨慎再谨慎')
                     i = i + 1
                     time.sleep(3)
 
@@ -63,7 +63,7 @@ def execute():
                 j = 0
                 for j in [0] :
                     xinhao = xinhao + codeItem + '卖出信号出现：' +  maichuresult_D + ' ' + maichuresult_60 + ' ' + maichuresult_bl_60 + ' '
-                    email_util.sendMail(codeItem + '卖出信号出现：' +  maichuresult_60 + ' ' + maichuresult_bl_60, '卖出，果断果断再果断')
+                    email_util.sendMail(codeItem + '卖出信号出现：' +  maichuresult_60 + ' ' + maichuresult_bl_60, codeItem + '卖出，果断果断再果断')
                     j = j + 1
                     time.sleep(3)
 
