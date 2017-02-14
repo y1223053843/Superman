2f69a79e3aa75c394d4eb40c10af28665112df63
yangjinpeng <yangjinpeng@maijia.com>
2017-02-14 13:32:38 +0800

    【Superman】文件修改

---
+++ x04_code_json_mongo_email.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# 启动进程
+PATH=/root/anaconda2/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/root/bin
+export PATH
+cd /root/worksapce/Superman/com/alex/strategy
+nohup python x04_code_json_mongo_email.py > x04_code.log 2>&1 &
