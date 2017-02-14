bdea519b446a81ce8077d546e63ea458fca287ca
yangjinpeng <yangjinpeng@maijia.com>
2017-02-14 19:16:56 +0800

    【Superman】文件修改

---
+++ 6_gold_code_json_mongo_email_param.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# 启动进程
+PATH=/root/anaconda2/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/root/bin
+export PATH
+cd /root/worksapce/Superman/com/alex/strategy
+nohup python gold_code_json_mongo_email.py 5 > gold_code_param.log 2>&1 &
