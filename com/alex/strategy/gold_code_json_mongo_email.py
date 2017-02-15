1fbd74d69c3d571b285faf6e86b090871f8240e3
yangjinpeng <yangjinpeng@maijia.com>
2017-02-15 20:37:08 +0800

    【Superman】文件修改

--- com/alex/strategy/gold_code_json_mongo_email.py
+++ com/alex/strategy/gold_code_json_mongo_email.py
@@ -49,11 +49,11 @@ def execute():
             #print  mairuresult_D + ' ' + mairuresult_bl_D
             jsonDic['052时买入信息'] =  mairuresult_60 + ' ' + mairuresult_bl_60
 
-            if (mairuresult_60  != '' or mairuresult_bl_60 != ''):
+            if (mairuresult_D !='' and (mairuresult_60  != '' or mairuresult_bl_60 != '')):
                 i = 0
                 for i in [0] :
-                    xinhao = xinhao + codeItem + '买入信号出现：' +  mairuresult_D + ' ' + mairuresult_60 + ' ' + mairuresult_bl_60 + ' '
-                    email_util.sendMail(codeItem + '买入信号出现：' +  mairuresult_60 + ' ' + mairuresult_bl_60, codeItem + '买入，谨慎谨慎再谨慎')
+                    xinhao = xinhao + codeItem + '买入信号出现：' +  mairuresult_D + '<br> ' + mairuresult_60 + '<br>' + mairuresult_bl_60 + ' '
+                    email_util.sendMail(codeItem + '买入信号出现：' + mairuresult_D + '<br>' + mairuresult_60 + '<br>' + mairuresult_bl_60, codeItem + '买入，谨慎谨慎再谨慎')
                     i = i + 1
                     time.sleep(3)
 
@@ -62,8 +62,8 @@ def execute():
             if (maichuresult_60 != '' or  maichuresult_bl_60 != ''):
                 j = 0
                 for j in [0] :
-                    xinhao = xinhao + codeItem + '卖出信号出现：' +  maichuresult_D + ' ' + maichuresult_60 + ' ' + maichuresult_bl_60 + ' '
-                    email_util.sendMail(codeItem + '卖出信号出现：' +  maichuresult_60 + ' ' + maichuresult_bl_60, codeItem + '卖出，果断果断再果断')
+                    xinhao = xinhao + codeItem + '卖出信号出现：' +  maichuresult_D + '<br>' + maichuresult_60 + '<br>' + maichuresult_bl_60 + ' '
+                    email_util.sendMail(codeItem + '卖出信号出现：' +  maichuresult_D + '<br>' +maichuresult_60 + '<br>' + maichuresult_bl_60, codeItem + '卖出，果断果断再果断')
                     j = j + 1
                     time.sleep(3)
 
