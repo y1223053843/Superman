7a5018a0792f13600fe7025e289e1eb4d49b73d0
yangjinpeng <yangjinpeng@maijia.com>
2017-02-21 12:45:05 +0800

    【Superman】文件修改

--- x02_code_json_mongo_email_param.sh
+++ x02_code_json_mongo_email_param.sh
@@ -3,4 +3,4 @@
 PATH=/root/anaconda2/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/root/bin
 export PATH
 cd /root/worksapce/Superman/com/alex/strategy
-nohup python x02_code_json_mongo_email_param.py 5 > x02_code_param.log 2>&1 &
+nohup python x02_code_json_mongo_email.py 5 > x02_code_param.log 2>&1 &
