3cf8c3989c9ed9f14c0f7cc518e1c1408b9417ee
yangjinpeng <yangjinpeng@maijia.com>
2017-04-11 14:40:17 +0800

    【Superman】文件修改

--- com/alex/strategy/gold_code_json_mongo_email.py
+++ com/alex/strategy/gold_code_json_mongo_email.py
@@ -101,14 +101,14 @@ def execute():
 
         caozuo = ''
         if xinhao.__contains__('买入'):
-            caozuo = ',【操作】买入'
+            caozuo = '【操作】买入'
         elif xinhao.__contains__('卖出'):
-            caozuo = ',【操作】卖出'
+            caozuo = '【操作】卖出'
 
         caozuo2 = ''
         if xinhao.__contains__('V型翻转'):
-            caozuo = ',【操作】买入点'
-            toDataFrame_param_content({}, '★★★★★My_Code_JSON_Mongo_' + time.strftime('%Y-%m-%d_%H:%M', time.localtime(time.time())) + '#【长期关注】'+ common.gupiaomingcheng(codeItem) + caozuo + caozuo2 +',【主题】黄金贵金属#',xinhao + xiaomowang, collectionName)
+            caozuo = '【操作】买入点'
+            toDataFrame_param_content({}, '★★★★★My_Code_JSON_Mongo_' + time.strftime('%Y-%m-%d_%H:%M', time.localtime(time.time())) + '#' + caozuo + caozuo2 +',【主题】黄金贵金属#',xinhao + xiaomowang, collectionName)
         elif xinhao.__contains__('下降1') or xinhao.__contains__('上穿'):
             caozuo = ',【操作】卖出点'
             #toDataFrame_param_content({}, '★★★★★My_Code_JSON_Mongo_' + time.strftime('%Y-%m-%d_%H:%M', time.localtime(time.time())) + '#【长期关注】'+ common.gupiaomingcheng(codeItem) + caozuo + caozuo2 +'#',xinhao, collectionName)
