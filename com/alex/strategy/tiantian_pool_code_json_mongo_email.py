6ab4aac4dd41f580e8cc33c26cc624a5b1e73f63
yangjinpeng <yangjinpeng@maijia.com>
2017-04-22 08:07:05 +0800

    【Superman】文件修改

--- com/alex/strategy/tiantian_pool_code_json_mongo_email.py
+++ com/alex/strategy/tiantian_pool_code_json_mongo_email.py
@@ -34,43 +34,8 @@ def execute(all_code_index, all_title):
     for codeItem in all_code_index:
         print time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())) + "=====" + codeItem
         try:
-
-            macd_30,macdsignal_30,macdhist_30,jsonResult_30,result_30,mairuresult_30,maichuresult_30  = MACD(codeItem,  '30')
-            macd_60,macdsignal_60,macdhist_60,jsonResult_60,result_60,mairuresult_60,maichuresult_60  = MACD(codeItem,  '60')
-            macd_D,macdsignal_D,macdhist_D,jsonResult_D,result_D,mairuresult_D,maichuresult_D  = MACD(codeItem,  'D')
-            macd_W,macdsignal_W,macdhist_W,jsonResult_W,result_W,mairuresult_W,maichuresult_W  = MACD(codeItem,  'W')
-            upperband_30, middleband_30, lowerband_30, jsonResult_b_30, result_bl_30,mairuresult_bl_30,maichuresult_bl_30 = BBANDS(codeItem, '30')
-            upperband_60, middleband_60, lowerband_60, jsonResult_b_60, result_bl_60,mairuresult_bl_60,maichuresult_bl_60 = BBANDS(codeItem, '60')
-            upperband_D, middleband_D, lowerband_D, jsonResult_b_D, result_bl_D,mairuresult_bl_D,maichuresult_bl_D = BBANDS(codeItem, 'D')
-            upperband_W, middleband_W, lowerband_W, jsonResult_b_W, result_bl_W,mairuresult_bl_W,maichuresult_bl_W = BBANDS(codeItem, 'W')
-
             query_result = [codeItem]
             strategy002(query_result, '【主题】【选】' + all_code.loc[codeItem,'hangye'], collectionName)
-            jsonDic = {}
-            jsonDic['90_Time'] = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
-            jsonDic['02Code'] = codeItem
-            #jsonDic['02Code2'] = '_' + codeItem
-            jsonDic['03Name'] = common.gupiaomingcheng(codeItem)
-            jsonDic['04Hangye'] = all_code.loc[codeItem,'hangye']
-            jsonDic['05涨跌幅'] = common.zhangdiefu(codeItem)
-            jsonDic['061日买入信息'] = mairuresult_D + ' ' + mairuresult_bl_D
-            jsonDic['062时买入信息'] =  mairuresult_60 + ' ' + mairuresult_bl_60
-            jsonDic['07卖出信息'] = maichuresult_60 + ' ' + maichuresult_D + ' ' + maichuresult_bl_60 + ' ' + maichuresult_bl_D
-            jsonDic['08上升通道'] = result_D + ' ' + result_bl_D
-            #jsonDic['验证_MACD_30'] =  '%.3f' % macd_30[-1] + '_' +  '%.3f' % macd_30[-2] + '_' +  '%.3f' % macd_30[-3]
-            #jsonDic['验证_MACD_60'] =  '%.3f' % macd_60[-1] + '_' +  '%.3f' % macd_60[-2] + '_' +  '%.3f' % macd_60[-3]
-            #jsonDic['验证_MACD_D'] =  '%.3f' % macd_D[-1] + '_' +  '%.3f' % macd_D[-2] + '_' +  '%.3f' % macd_D[-3]
-            #jsonDic['验证_MACD_W'] =  '%.3f' % macd_W[-1] + '_' +  '%.3f' % macd_W[-2] + '_' +  '%.3f' % macd_W[-3]
-            #jsonDic['验证_布林_30'] =  '%.3f' % middleband_30[-1] + '_' +  '%.3f' % middleband_30[-2] + '_' +  '%.3f' % middleband_30[-3]
-            #jsonDic['验证_布林_60'] =  '%.3f' % middleband_60[-1] + '_' +  '%.3f' % middleband_60[-2] + '_' +  '%.3f' % middleband_60[-3]
-            #jsonDic['验证_布林_D'] =  '%.3f' % middleband_D[-1] + '_' +  '%.3f' % middleband_D[-2] + '_' +  '%.3f' % middleband_D[-3]
-            #jsonDic['验证_布林_W'] =  '%.3f' % middleband_W[-1] + '_' +  '%.3f' % middleband_W[-2] + '_' +  '%.3f' % middleband_W[-3]
-
-            jsonParam = dict(jsonResult_30.items() + jsonResult_60.items() + jsonResult_D.items()
-                             + jsonResult_W.items() + jsonResult_b_30.items() + jsonResult_b_60.items()
-                             + jsonResult_b_D.items() + jsonResult_b_W.items() + jsonDic.items())
-
-            insertRecord_param(jsonParam, collectionName)
         except (IOError, TypeError, NameError, IndexError,Exception) as e:
             logging.error("error:" + codeItem)
             print e
