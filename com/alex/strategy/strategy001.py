adefc442081b50f394f41ae636269f6370a52709
yangjinpeng <yangjinpeng@maijia.com>
2017-04-24 16:20:28 +0800

    【Superman】文件修改

--- com/alex/strategy/strategy001.py
+++ com/alex/strategy/strategy001.py
@@ -54,9 +54,9 @@ def strategy001(query_result, zhuti, collectionName):
                 i = 0
                 for i in [0] :
                     xinhao = xinhao + codeItem + '买入信号出现：<br>' +  mairuresult_D + '<br> ' + mairuresult_60 + '<br>' + mairuresult_bl_60 + ' '
-                    email_util.sendMail(codeItem + '买入信号出现：<br>' + mairuresult_D + '<br>' + mairuresult_60 + '<br>' + mairuresult_bl_60, codeItem + '买入，谨慎谨慎再谨慎')
+                    #email_util.sendMail(codeItem + '买入信号出现：<br>' + mairuresult_D + '<br>' + mairuresult_60 + '<br>' + mairuresult_bl_60, codeItem + '买入，谨慎谨慎再谨慎')
                     i = i + 1
-                    time.sleep(1)
+                    #time.sleep(1)
 
             jsonDic['06卖出信息'] = maichuresult_60 + ' ' + maichuresult_D + ' ' + maichuresult_bl_60 + ' ' + maichuresult_bl_D
 
@@ -65,9 +65,9 @@ def strategy001(query_result, zhuti, collectionName):
                 j = 0
                 for j in [0] :
                     xinhao = xinhao + codeItem + '卖出信号出现：<br>' +  maichuresult_D + '<br>' + maichuresult_60 + '<br>' + maichuresult_bl_60 + ' '
-                    email_util.sendMail(codeItem + '卖出信号出现：<br>' +  maichuresult_D + '<br>' +maichuresult_60 + '<br>' + maichuresult_bl_60, codeItem + '卖出，果断果断再果断')
+                    #email_util.sendMail(codeItem + '卖出信号出现：<br>' +  maichuresult_D + '<br>' +maichuresult_60 + '<br>' + maichuresult_bl_60, codeItem + '卖出，果断果断再果断')
                     j = j + 1
-                    time.sleep(1)
+                    #time.sleep(1)
 
             # 验证
             jsonDic['验证_MACD_30'] =  '%.3f' % macd_30[-1] + '_' +  '%.3f' % macd_30[-2] + '_' +  '%.3f' % macd_30[-3]
